8a28a8aa76aad5518d2fe68904f4893ea773dfef
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-05-02 18:32:20 +0700

    Polygon buffer formatting.

--- polygon_buffer.cpp
+++ polygon_buffer.cpp
@@ -1,6 +1,3 @@
-#ifndef POLYGON_BUFFER_CPP
-#define POLYGON_BUFFER_CPP
-
 #include "polygon_buffer.hpp"
 
 #define BUFFER_NOT_CREATED 0xffffffff
@@ -24,24 +21,24 @@ void r_PolygonBuffer::VertexData( const void* data, unsigned int d_size, unsigne
 		glGenVertexArrays( 1, &v_array_object_ );
 	glBindVertexArray( v_array_object_ );
 
-    if( v_buffer_ == BUFFER_NOT_CREATED )
-        glGenBuffers( 1, &v_buffer_ );
+	if( v_buffer_ == BUFFER_NOT_CREATED )
+		glGenBuffers( 1, &v_buffer_ );
 
-    glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
-    glBufferData( GL_ARRAY_BUFFER, d_size, data, GL_STATIC_DRAW );
+	glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
+	glBufferData( GL_ARRAY_BUFFER, d_size, data, GL_STATIC_DRAW );
 
-    vertex_data_size_= d_size;
-    vertex_size_= v_size;
+	vertex_data_size_= d_size;
+	vertex_size_= v_size;
 }
 
 void r_PolygonBuffer::VertexSubData( const void* data, unsigned int d_size, unsigned int shift )
 {
-    if( v_buffer_ == BUFFER_NOT_CREATED || v_array_object_ == BUFFER_NOT_CREATED )
-        return;
+	if( v_buffer_ == BUFFER_NOT_CREATED || v_array_object_ == BUFFER_NOT_CREATED )
+		return;
 	glBindVertexArray( v_array_object_ );
 
-    glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
-    glBufferSubData( GL_ARRAY_BUFFER, shift, d_size, data );
+	glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
+	glBufferSubData( GL_ARRAY_BUFFER, shift, d_size, data );
 }
 
 void r_PolygonBuffer::IndexData( const void* data, unsigned int size, GLenum d_type, GLenum p_type )
@@ -50,30 +47,31 @@ void r_PolygonBuffer::IndexData( const void* data, unsigned int size, GLenum d_t
 		glGenVertexArrays( 1, &v_array_object_ );
 	glBindVertexArray( v_array_object_ );
 
-    if( i_buffer_ == BUFFER_NOT_CREATED )
-        glGenBuffers( 1, &i_buffer_ );
+	if( i_buffer_ == BUFFER_NOT_CREATED )
+		glGenBuffers( 1, &i_buffer_ );
 
-    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, i_buffer_ );
-    glBufferData( GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, i_buffer_ );
+	glBufferData( GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW );
 
-    index_data_size_= size;
-    index_data_type_= d_type;
-    primitive_type_= p_type;
+	index_data_size_= size;
+	index_data_type_= d_type;
+	primitive_type_= p_type;
 }
 
 void r_PolygonBuffer::IndexSubData( const void* data, unsigned int size, int shift )
 {
-    if( i_buffer_ == BUFFER_NOT_CREATED || v_array_object_ == BUFFER_NOT_CREATED  )
-        return;
+	if( i_buffer_ == BUFFER_NOT_CREATED || v_array_object_ == BUFFER_NOT_CREATED  )
+		return;
 
-    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, i_buffer_ );
-    glBufferSubData( GL_ELEMENT_ARRAY_BUFFER, shift, size, data );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, i_buffer_ );
+	glBufferSubData( GL_ELEMENT_ARRAY_BUFFER, shift, size, data );
 }
 
 void r_PolygonBuffer::Bind() const
 {
-    if( v_array_object_ != BUFFER_NOT_CREATED )
-        glBindVertexArray( v_array_object_ );
+	if( v_array_object_ != BUFFER_NOT_CREATED )
+		glBindVertexArray( v_array_object_ );
+	//glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, i_buffer_ );
 }
 
 void r_PolygonBuffer::Destroy()
@@ -97,49 +95,46 @@ void r_PolygonBuffer::Destroy()
 
 void r_PolygonBuffer::Show() const
 {
-    Bind();
+	Bind();
 
-    int s;
-    if( i_buffer_ == BUFFER_NOT_CREATED )
-    {
-        if( v_buffer_ == BUFFER_NOT_CREATED )
-            return;
-        s= vertex_data_size_ / vertex_size_;
-        glDrawArrays( primitive_type_, 0, s );
-    }
-    else
-    {
-        if( v_buffer_ == BUFFER_NOT_CREATED )
-            return;
-        s= index_data_type_ == GL_UNSIGNED_INT ? 4 : 2;
-        s= index_data_size_ / s;
-        glDrawElements( primitive_type_, s, index_data_type_, NULL );
-    }
+	int s;
+	if( i_buffer_ == BUFFER_NOT_CREATED )
+	{
+		if( v_buffer_ == BUFFER_NOT_CREATED )
+			return;
+		s= vertex_data_size_ / vertex_size_;
+		glDrawArrays( primitive_type_, 0, s );
+	}
+	else
+	{
+		if( v_buffer_ == BUFFER_NOT_CREATED )
+			return;
+		s= index_data_type_ == GL_UNSIGNED_INT ? 4 : 2;
+		s= index_data_size_ / s;
+		glDrawElements( primitive_type_, s, index_data_type_, NULL );
+	}
 }
-
 
 void r_PolygonBuffer::VertexAttribPointer( int v_attrib, int components, GLenum type, bool normalize, int shift )
 {
-    if( v_array_object_ == BUFFER_NOT_CREATED || v_buffer_ == BUFFER_NOT_CREATED )
-        return;
+	if( v_array_object_ == BUFFER_NOT_CREATED || v_buffer_ == BUFFER_NOT_CREATED )
+		return;
 
-    glBindVertexArray( v_array_object_ );
-    glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
+	glBindVertexArray( v_array_object_ );
+	glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
 
 	glEnableVertexAttribArray( v_attrib );
-    glVertexAttribPointer( v_attrib, components, type, normalize, vertex_size_, (void*) shift );
+	glVertexAttribPointer( v_attrib, components, type, normalize, vertex_size_, (void*) shift );
 }
 
 void r_PolygonBuffer::VertexAttribPointerInt( int v_attrib, int components, GLenum type, int shift )
 {
-   	if( v_array_object_ == BUFFER_NOT_CREATED || v_buffer_ == BUFFER_NOT_CREATED )
-        return;
+	if( v_array_object_ == BUFFER_NOT_CREATED || v_buffer_ == BUFFER_NOT_CREATED )
+		return;
 
-    glBindVertexArray( v_array_object_ );
-    glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
+	glBindVertexArray( v_array_object_ );
+	glBindBuffer( GL_ARRAY_BUFFER, v_buffer_ );
 
 	glEnableVertexAttribArray( v_attrib );
-    glVertexAttribIPointer( v_attrib, components, type, vertex_size_, (void*) shift );
+	glVertexAttribIPointer( v_attrib, components, type, vertex_size_, (void*) shift );
 }
-
-#endif//POLYGON_BUFFER_CPP
--- polygon_buffer.hpp
+++ polygon_buffer.hpp
@@ -1,20 +1,18 @@
 #pragma once
-
 #include "panzer_ogl_lib.hpp"
 
-#define BUFFER_NOT_CREATED 0xffffffff
 class r_PolygonBuffer
 {
 public:
     r_PolygonBuffer();
 	~r_PolygonBuffer();
 
-	void VertexData      ( const void* data, unsigned int d_size, unsigned int v_size );
-	void VertexSubData   ( const void* data, unsigned int d_size, unsigned int shift );
+	void VertexData( const void* data, unsigned int d_size, unsigned int v_size );
+	void VertexSubData( const void* data, unsigned int d_size, unsigned int shift );
 
-	void IndexData       ( const void* data, unsigned int size, GLenum d_type, GLenum p_type );
-	void IndexSubData    ( const void* data, unsigned int size, int shift );
-	void VertexAttribPointer   ( int v_attrib, int components, GLenum type, bool normalize, int shift );
+	void IndexData( const void* data, unsigned int size, GLenum d_type, GLenum p_type );
+	void IndexSubData( const void* data, unsigned int size, int shift );
+	void VertexAttribPointer( int v_attrib, int components, GLenum type, bool normalize, int shift );
 	void VertexAttribPointerInt( int v_attrib, int components, GLenum type, int stride );
 
 	void Bind() const;
@@ -39,6 +37,4 @@ private:
 
 	GLuint v_buffer_, i_buffer_;
 	GLuint v_array_object_;
-
-
 };
