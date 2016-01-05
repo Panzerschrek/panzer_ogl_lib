fe24c13c826b5b92847ae3c07f52c6b83000d8f5
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-01-05 12:09:38 +0700

    Dovedenije do uma r_OGLState.

--- ogl_state_manager.hpp
+++ ogl_state_manager.hpp
@@ -2,18 +2,22 @@
 
 #include "panzer_ogl_lib.hpp"
 
-class r_OGLState
+class r_OGLState final
 {
+	friend class r_OGLStateManager;
+
 public:
-
+	// Default state variables.
+	// Not equal to OpenGL initial state.
+	// Use this, if has no difference, what to use.
 	static const float default_clear_color[4];
-	static const float default_clear_depth;
-	static const GLenum default_cull_face_mode;
-	static const bool default_depth_mask;
+	static constexpr const float default_clear_depth= 1.0f;
+	static constexpr const GLenum default_cull_face_mode= GL_FRONT;
+	static constexpr const bool default_depth_mask= true;
 
-	r_OGLState(){}
-	~r_OGLState(){}
-	//constructor for full initialazing
+	// State on OpenGL startup.
+	r_OGLState();
+
 	r_OGLState(
 		bool blend, bool cull_face, bool depth_test, bool program_point_size,
 		const GLenum* blend_func,
@@ -22,29 +26,24 @@ public:
 		GLenum cull_face_mode= default_cull_face_mode,
 		bool depth_mask= default_depth_mask );
 
-	// State on OpenGL startup
-	void InitialState();
+protected:
+	bool blend_;
+	bool cull_face_;
+	bool depth_test_;
+	bool program_point_size_;
 
-	bool blend;
-	bool cull_face;
-	bool depth_test;
-	bool program_point_size;
-
-	GLenum blend_func[2];
-	float clear_color[4];
-	float clear_depth;
-	GLenum cull_face_mode;
-	bool depth_mask;
-	GLenum depth_func;
+	GLenum blend_func_[2];
+	float clear_color_[4];
+	float clear_depth_;
+	GLenum cull_face_mode_;
+	bool depth_mask_;
+	GLenum depth_func_;
 };
 
-class r_OGLStateManager
+class r_OGLStateManager final
 {
 public:
-	static void SetState( const r_OGLState& state );//FORCE set state
 	static void UpdateState( const r_OGLState& state );
-
-	static const r_OGLState& GetState();
 
 private:
 	static r_OGLState state_;
