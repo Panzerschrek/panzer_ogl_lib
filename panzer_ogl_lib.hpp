4d973c83fee7f038b2df9359d38496f0b9ba6d1c
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-02-10 11:19:53 +0200

    Fixed compilation on macOS

--- panzer_ogl_lib.hpp
+++ panzer_ogl_lib.hpp
@@ -5,6 +5,10 @@
 #include <windows.h>
 #endif // _MSC_VER
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else // !__APPLE__
+
 #include <GL/gl.h>
 
 // Catch system gl.h OpenGL version.
@@ -22,6 +26,8 @@
 #ifdef GL_VERSION_1_4
 #define R_SYSTEM_GL_VERSION_1_4
 #endif
+
+#endif // __APPLE__
 
 #include "glcorearb.h"
 
