ae110a7683e49e802efcf2cb2e59f0cc67981e08
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-06-30 10:58:29 +0700

    Kostyli dlä sborki pod Android.

--- panzer_ogl_lib.hpp
+++ panzer_ogl_lib.hpp
@@ -5,7 +5,11 @@
 #include <windows.h>
 #endif // _MSC_VER
 
+#ifdef __ANDROID__
+#include <GLES/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 
 // Catch system gl.h OpenGL version.
 // glcorearb.h redefines them all.
@@ -22,7 +26,13 @@
 #ifdef GL_VERSION_1_4
 #define R_SYSTEM_GL_VERSION_1_4
 #endif
+#ifdef GL_VERSION_ES_CM_1_0
+#define R_SYSTEM_GL_VERSION_ES
+#endif
 
+#ifdef __ANDROID__
+#define GL_VERSION_1_5
+#endif
 #include "glcorearb.h"
 
 #include "func_declarations.hpp"
